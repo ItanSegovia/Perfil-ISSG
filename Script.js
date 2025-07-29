246b11a31810cd5fb64ac81b8963c543f5574d95
ItanSegovia <al44653@un.edu.mx>
2025-07-29 10:19:34 -0600

    hice un css más sencillo y agregue el script

--- Script.js
+++ Script.js
@@ -4,3 +4,17 @@ const tarjeta = document.querySelector('.perfil-card');
 const nombre = document.getElementById('nombre');
 const ocupacion = document.getElementById('ocupacion');
 
+let esMasculino = true;
+
+tarjeta.addEventListener('click', () => {
+  if (esMasculino) {
+    nombre.textContent = 'Itana Segovia';
+    ocupacion.textContent = 'Desarrolladora Web';
+    alert('Se cambió el perfil a femenino');
+  } else {
+    nombre.textContent = 'Itan Segovia';
+    ocupacion.textContent = 'Desarrollador Web';
+    alert('Se cambió el perfil a masculino');
+  }
+  esMasculino = !esMasculino;
+});

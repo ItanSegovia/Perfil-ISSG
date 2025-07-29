eae409d4bde9191511b2096aff17bdb925795404
ItanSegovia <al44653@un.edu.mx>
2025-07-29 10:08:52 -0600

    agregue foto y asi

---
+++ Script.js
@@ -0,0 +1,6 @@
+document.getElementById('año').textContent = new Date().getFullYear();
+
+const tarjeta = document.querySelector('.perfil-card');
+const nombre = document.getElementById('nombre');
+const ocupacion = document.getElementById('ocupacion');
+
